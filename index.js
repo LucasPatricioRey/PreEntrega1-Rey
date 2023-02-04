e98bf0091be9f6cf002a176d1b0db5ce0ebdab56
LucasPatricioRey <lucas1998z@hotmail.com>
2023-02-04 11:03:10 -0300

    final

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function CalcularPromedio() {
-    let nombre= prompt("Ingrese el nombre del alumno")
-    let nota1 = parseInt(prompt("Ingrese nota del primer cuatrimestre"));
-    let nota2 = parseInt(prompt("Ingrese nota del segundo cuatrimestre"));
-    let promedio = (nota1 + nota2) / 2;
+    let nombre= prompt("Ingrese el nombre del alumno");
+    let notas;
+    let promedio;
+    
+    while(nombre != "salir") {
+        let suma= 0;
+        let nnotas= parseInt(prompt("Ingrese la cantidad de notas a promediar"));
+    
+        for (i=0; i< nnotas; i++){
+            notas= parseInt(prompt(`digita la nota ` + i));
+            suma = suma + notas;
+        }
+        promedio = suma / nnotas;
+        
+    
     if (promedio >= 7) {
-        alert(`el alumno ${nombre} esta aprobado`)
+        alert(`el alumno ${nombre} esta aprobado con una nota de ${promedio}`);
     }
     else {
-        alert(`el alumno ${nombre} esta desaprobado`)
+        alert(`el alumno ${nombre} esta desaprobado con una nota de ${promedio}`);
     }
+    nombre= prompt("Ingrese el nombre de otro alumno o escriba salir");
+}
 }
 CalcularPromedio()
